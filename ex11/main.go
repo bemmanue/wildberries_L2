7329a29189cb556654267f8ed2e2f895db19b9ae
bemmanue <myaushenka@gmail.com>
2023-05-22 23:55:32 +0300

    ok

--- ex11/main.go
+++ ex11/main.go
@@ -1,19 +1,118 @@
 package main
 
 import (
-	"bytes"
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		var b bytes.Buffer
-		req.Body.Read(b.Bytes())
+type Server struct {
+	router *http.ServeMux
+}
 
-		io.WriteString(w, "Hello")
+func (s *Server) start(addr string) error {
+	s.configureRouter()
+
+	return http.ListenAndServe(addr, s.router)
+}
+
+func (s *Server) configureRouter() {
+	s.router.HandleFunc("/create_event", s.CreateEvent())
+	s.router.HandleFunc("/update_event", s.UpdateEvent())
+	s.router.HandleFunc("/delete_event", s.DeleteEvent())
+	s.router.HandleFunc("/events_for_day", s.EventsForDay())
+	s.router.HandleFunc("/events_for_week", s.EventsForWeek())
+	s.router.HandleFunc("/events_for_month", s.EventsForMonth())
+}
+
+func (s *Server) CreateEvent() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+
+		default:
+			sendError(w, http.StatusNotFound, "Status Not Found")
+		}
 	}
-	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func sendError(w http.ResponseWriter, code int, msg string) {
+	doc := struct {
+		Error string `json:"error"`
+	}{
+		Error: msg,
+	}
+
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(doc)
+}
+
+func (s *Server) UpdateEvent() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func (s *Server) DeleteEvent() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func (s *Server) EventsForDay() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func (s *Server) EventsForWeek() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func (s *Server) EventsForMonth() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func newServer() *Server {
+	return &Server{
+		router: http.NewServeMux(),
+	}
+}
+
+func main() {
+	server := newServer()
+
+	if err := server.start(":8080"); err != nil {
+		log.Fatalln(err)
+	}
 }
