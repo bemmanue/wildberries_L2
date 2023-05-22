0d134857a13756afc44adec1c4c6bc8a1e8926c5
bemmanue <myaushenka@gmail.com>
2023-05-22 18:38:37 +0300

    ok

---
+++ ex10/go.mod
@@ -0,0 +1,5 @@
+module ex10
+
+go 1.20
+
+require golang.org/x/sys v0.8.0 // indirect
--- ex10/main.go
+++ ex10/main.go
@@ -6,35 +6,79 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
+// shutdown закрывает соединение и завершает программу
+// в случае получения сигналов SIGQUIT, SIGTERM, SIGINT
+func shutdown(conn net.Conn) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+
+	<-ch
+
+	conn.Close()
+
+	fmt.Println("Program shutdown")
+	os.Exit(0)
+}
+
+// telnet устанавливает соединение с сервером
+func telnet(address string, timeout time.Duration) error {
+	// подключаемся к серверу
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return err
+	}
+
+	// закрываем соединение в случае получения сигнала SIGQUIT
+	go shutdown(conn)
+
+	// общаемся с сервером
+	for {
+		// считываем сообщение из стандартного ввода
+		reader := bufio.NewReader(os.Stdin)
+		request, err := reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+
+		// отправляем сообщение на сервер
+		if _, err := fmt.Fprintf(conn, request+"\n"); err != nil {
+			return err
+		}
+
+		// прослушиваем ответ
+		response, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			return err
+		}
+
+		// печатаем ответ в стандартный вывод
+		fmt.Println(response)
+	}
+}
+
 func main() {
-	//timeout := flag.Int("timeout", 0, "Установить таймаут")
+	// устанавливаем флаги
+	timeout := flag.Duration("timeout", 10*time.Second, "Установить таймаут")
 	flag.Parse()
 
-	host := flag.Arg(0)
-	port := flag.Arg(1)
-	address := fmt.Sprintf("%s:%s", host, port)
-	fmt.Println(address)
-
-	//Подключаемся к сокету
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+	// проверяем аргументы
+	if len(flag.Args()) != 2 {
+		fmt.Fprintln(os.Stderr, "usege: ./telnet [-timeout t] host port")
 		os.Exit(1)
 	}
 
-	for {
-		// Чтение входных данных от stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
+	// подготавливаем параметры
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := fmt.Sprintf("%s:%s", host, port)
 
-		// Отправляем в socket
-		fmt.Fprintf(conn, text+"\n")
-
-		// Прослушиваем ответ
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + message)
+	if err := telnet(address, *timeout); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 }
