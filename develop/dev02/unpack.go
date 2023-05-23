076d4d254ecd3507a08424f1d17aaade7af17afd
bemmanue <myaushenka@gmail.com>
2023-05-24 01:44:36 +0300

    added comments

--- develop/dev02/unpack.go
+++ develop/dev02/unpack.go
@@ -1,29 +1,48 @@
 package main
 
+/*
+=== Задача на распаковку ===
+Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
+	- "a4bc2d5e" => "aaaabccddddde"
+	- "abcd" => "abcd"
+	- "45" => "" (некорректная строка)
+	- "" => ""
+Дополнительное задание: поддержка escape - последовательностей
+	- qwe\4\5 => qwe45 (*)
+	- qwe\45 => qwe44444 (*)
+	- qwe\\5 => qwe\\\\\ (*)
+В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
+Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
+*/
+
 import (
 	"errors"
+	"fmt"
 )
 
 var (
 	ErrInvalidString = errors.New("invalid string")
 )
 
+// State описывает тип символа: обычный символ, цифра, экранирующий символ
 type State struct {
 	symbol bool
 	digit  bool
 	escape bool
 }
 
-func repeat(c rune, count int) []rune {
-	res := make([]rune, count)
+// repeat возвращает последовательность из n символов с
+func repeat(c rune, n int) []rune {
+	res := make([]rune, n)
 
-	for i := 0; i < count; i++ {
+	for i := 0; i < n; i++ {
 		res[i] = c
 	}
 
 	return res
 }
 
+// isDigit проверяет, является ли символ цифрой
 func isDigit(c rune) bool {
 	if c >= '0' && c <= '9' {
 		return true
@@ -31,6 +50,7 @@ func isDigit(c rune) bool {
 	return false
 }
 
+// getCurrentState определяет принадлежность тип символа
 func getCurrentState(c rune, previous State) State {
 	current := State{}
 
@@ -51,6 +71,7 @@ func getCurrentState(c rune, previous State) State {
 	return current
 }
 
+// Unpack распаковывает строку
 func Unpack(s string) (string, error) {
 	var result []rune
 
@@ -62,16 +83,21 @@ func Unpack(s string) (string, error) {
 		return "", nil
 	}
 
+	// Определяем тип первого символа
 	current = getCurrentState(str[0], previous)
 
+	// Если первый символ является цифрой, то строка невалидна
 	if current.digit {
 		return "", ErrInvalidString
 	}
 
+	// Если первый символ не является экранирующим символом, добавляем его в результат
 	if !current.escape {
 		result = append(result, str[0])
 	}
 
+	// проходим по всем элементам строки и в зависимости
+	// от типа символа определяем дальнейшие действия
 	for i := 1; i < len(str); i++ {
 		previous = current
 		current = getCurrentState(str[i], previous)
@@ -81,7 +107,6 @@ func Unpack(s string) (string, error) {
 		} else if current.digit {
 			count := int(str[i] - '0')
 			rep := repeat(result[len(result)-1], count)
-
 			result = append(result[:len(result)-1], rep...)
 		} else if current.escape {
 
@@ -90,9 +115,19 @@ func Unpack(s string) (string, error) {
 		}
 	}
 
+	// Если последний символ является экранирующим, то строка невалидна
 	if current.escape {
 		return "", ErrInvalidString
 	}
 
 	return string(result), nil
+}
+
+func main() {
+	result, err := Unpack(`a4bc2d5e`)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(result)
+	}
 }
