6f83cc1f59955dda5ddea14fb499295f1118c39f
bemmanue <myaushenka@gmail.com>
2023-05-24 16:15:43 +0300

    done

--- pattern/03_visitor.go
+++ pattern/03_visitor.go
@@ -1,5 +1,103 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+	Реализовать паттерн «посетитель».
+Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+	https://en.wikipedia.org/wiki/Visitor_pattern
+*/
+
+/*
+Посетитель - паттерн поведения объектов.
+Он описывает операцию, выполняемую с каждым объектом из некоторой структуры.
+Паттерн посетитель позволяет определить новую операцию, не изменяя классы этих объектов.
+
+Применимость паттерна:
+- в структуре присутствуют объекты многих классов с различными интерфейсами
+и есть необходимость выполнять над ними операции, зависящие от конкретных классов;
+- над объектами, входящими в состав структуры, надо выполнять разнообразные,
+не связанные между собой операции и вы не хотите «засорять» классы такими операциями;
+- классы, устанавливающие структуру объектов, изменяются редко,
+но новые операции над этой структурой добавляются часто.
+
+Плюсы и минусы:
+- упрощает добавление новых операций;
+- объединяет родственные операции и отсекает те, которые не имеют к ним отношения;
+- добавление новых классов ConcreteElement затруднено;
+- посещение различных иерархий классов;
+- аккумулирование состояния;
+- нарушение инкапсуляции.
+
+Примеры использования:
+- В компиляторе Smalltalk-80 имеется класс посетителя, который называется ProgramNodeEnumerator.
+В основном он применяется в алгоритмах анализа исходного текста программы
+и не используется ни для генерации кода, ни для красивой печати, хотя мог бы.
+*/
+
+type Visitor interface {
+	VisitRestaurant(p *Restaurant) string
+	VisitBar(p *Bar) string
+}
+
+type Place interface {
+	Accept(v Visitor) string
+}
+
+type People struct {
+}
+
+func (v *People) VisitRestaurant(p *Restaurant) string {
+	return p.Dinner()
+}
+
+func (v *People) VisitBar(p *Bar) string {
+	return p.Drink()
+}
+
+type City struct {
+	places []Place
+}
+
+func (c *City) Add(p Place) {
+	c.places = append(c.places, p)
+}
+
+func (c *City) Accept(v Visitor) string {
+	var result string
+	for _, p := range c.places {
+		result += p.Accept(v) + "\n"
+	}
+	return result
+}
+
+type Restaurant struct {
+}
+
+func (s *Restaurant) Accept(v Visitor) string {
+	return v.VisitRestaurant(s)
+}
+
+func (s *Restaurant) Dinner() string {
+	return "Have dinner"
+}
+
+type Bar struct {
+}
+
+func (p *Bar) Accept(v Visitor) string {
+	return v.VisitBar(p)
+}
+
+func (p *Bar) Drink() string {
+	return "Drink cocktail"
+}
+
+func main() {
+	city := new(City)
+
+	city.Add(&Restaurant{})
+	city.Add(&Bar{})
+
+	fmt.Println(city.Accept(&People{}))
 }
--- pattern/04_command.go
+++ pattern/04_command.go
@@ -1,5 +1,93 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+	Реализовать паттерн «команда».
+Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+	https://en.wikipedia.org/wiki/Command_pattern
+*/
+
+/*
+Команда - паттерн поведения объектов.
+Он инкапсулирует запрос как объект, позволяя тем самым задавать параметры клиентов
+для обработки соответствующих запросов, ставить запросы в очередь или протоколировать их,
+а также поддерживать отмену операций.
+
+Применимость паттерна:
+- когда необходимо параметризовать объекты выполняемым действием;
+- когда необходимо определять, ставить в очередь и выполнять запросы в разное время;
+- когда необходимо поддержать отмену операций;
+- когда необходимо поддержать протоколирование изменений,
+чтобы их можно было выполнить повторно после аварийной остановки системы;
+- когда необходимо структурировать систему на основе высокоуровневых операций, построенных из примитивных
+
+Плюсы и минусы:
+- позволяет добиться высокой гибкости при проектировании пользовательского интерфейса.
+
+Примеры использования:
+- в системе МасАрр [Арр89] команды широко применяются для реализации допускающих отмену операций.
+*/
+
+type Command interface {
+	Execute() string
+}
+
+type Wash struct {
+	receiver *Laundry
+}
+
+func (c *Wash) Execute() string {
+	return c.receiver.Wash()
+}
+
+type WringOut struct {
+	receiver *Laundry
+}
+
+func (c *WringOut) Execute() string {
+	return c.receiver.WringOut()
+}
+
+type Laundry struct {
+}
+
+func (r *Laundry) Wash() string {
+	return "Washing"
+}
+
+func (r *Laundry) WringOut() string {
+	return "Wringing out"
+}
+
+type Machine struct {
+	commands []Command
+}
+
+func (i *Machine) StoreCommand(command Command) {
+	i.commands = append(i.commands, command)
+}
+
+func (i *Machine) UnStoreCommand() {
+	if len(i.commands) != 0 {
+		i.commands = i.commands[:len(i.commands)-1]
+	}
+}
+
+func (i *Machine) Execute() string {
+	var result string
+	for _, command := range i.commands {
+		result += command.Execute() + "\n"
+	}
+	return result
+}
+
+func main() {
+	washingMachine := &Machine{}
+	laundry := &Laundry{}
+
+	washingMachine.StoreCommand(&Wash{receiver: laundry})
+	washingMachine.StoreCommand(&WringOut{receiver: laundry})
+
+	fmt.Println(washingMachine.Execute())
 }
